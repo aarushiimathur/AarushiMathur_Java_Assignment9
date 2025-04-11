48ccf3c3de90ae62a0d4dd89f70b7d52cc5b8c0d
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-04-12 02:13:15 +0530

    Create StudentOperation.java

---
+++ StudentOperation.java
@@ -0,0 +1,8 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class StudentOperation {
+    private static final String URL = "jdbc:mysql://localhost:3306/db_student";
+    private static final String USER = "root";
+    private static final String PASSWORD = "mysql2201";
+    
